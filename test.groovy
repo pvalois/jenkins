d53ee0741686965916d9ae45bb96157f278d7633
pascal valois <pascal.valois@free.fr>
2024-03-01 19:15:07 +0100

    FIX: test.groovy

--- test.groovy
+++ test.groovy
@@ -5,45 +5,39 @@ pipeline {
             timeout(time: 15, unit: 'MINUTES')
     }
 
-    env.DEPLOY_TO_LAST = input message: 'User input required',
-        parameters: [choice(name: 'Deploy to LAST', choices: 'hello\nbuild')]
+    stages {
+        stage('Hello') {
+          agent {
+              docker {
+                  image 'maven:3.9.3-eclipse-temurin-17'
+                  args  '-v /tmp:/tmp'
+                  reuseNode true
+              }
+          }
 
-    if (env.DEPLOY_TO_LAST == 'hello'){    
-
-      stages {
-          stage('Hello') {
+          steps {
+              sh 'hostname'
+              sh 'touch /tmp/bollocks'
+              echo 'Hello World'
+          }
+        }
+    
+        
+        stage('Build') {
             agent {
                 docker {
-                    image 'maven:3.9.3-eclipse-temurin-17'
-                    args  '-v /tmp:/tmp'
-                    reuseNode true
+                    image 'gradle:8.2.0-jdk17-alpine'
+                    // Run the container on the node specified at the
+                    // top-level of the Pipeline, in the same workspace,
+                    // rather than on a new node entirely:
+                   reuseNode true
                 }
             }
 
             steps {
-                sh 'hostname'
-                sh 'touch /tmp/bollocks'
-                echo 'Hello World'
-            }
-          }
+                sh 'gradle --version'
+           }
+        }
      }
-        
-    if (env.DEPLOY_TO_LAST == 'build'){    
-          stage('Build') {
-              agent {
-                  docker {
-                      image 'gradle:8.2.0-jdk17-alpine'
-                      // Run the container on the node specified at the
-                      // top-level of the Pipeline, in the same workspace,
-                      // rather than on a new node entirely:
-                     reuseNode true
-                  }
-              }
-
-              steps {
-                  sh 'gradle --version'
-             }
-          }
-       }
-    }
+  
 }
